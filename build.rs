0f09e5e5dbca86bfb9bb6a5fb3b810b020288e0b
Raphael Darley <raphael@raphaeldarley.com>
2023-09-01 23:35:52 +0100

    upgrade to beta10 and add conditional rocksdb comp

--- build.rs
+++ build.rs
@@ -1,5 +1,15 @@
 extern crate napi_build;
 
 fn main() {
+  let target = std::env::var("TARGET").unwrap();
+
+  if target == "x86_64-pc-windows-msvc"
+    || target == "x86_64-unknown-linux-gnu"
+    || target == "aarch64-linux-android"
+    || target == "armv7-linux-androideabi"
+  {
+    println!("cargo:rustc-cfg=rocksdb");
+  }
+
   napi_build::setup();
 }
