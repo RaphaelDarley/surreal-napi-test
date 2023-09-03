14add527c23aa2d7e80176a0eb161e01e5d35a4d
Raphael Darley <raphael@raphaeldarley.com>
2023-09-04 00:59:09 +0100

    fix artifacts script

--- move_artifacts.js
+++ move_artifacts.js
@@ -6,41 +6,50 @@ const npmDir = './npm';
 
 const outDirs = fs.readdirSync(npmDir);
 
-fs.readdir(sourceDir, (err, files) => {
-    if (err) {
-        console.error(`Could not list the directory: ${err}`);
+const binaries = collectNodeBinaries(sourceDir);
+console.log(binaries)
+
+binaries.forEach((file) => {
+    // console.log(file);
+    // const sourcePath = path.join(sourceDir, file);
+    console.log(`moving artifact: ${file}`);
+    const terms = file.split('.');
+    if (terms.pop() !== 'node') {
+        console.error(`non node file found: ${file}`);
         return;
     }
-    files.forEach((file) => {
-        // console.log(file);
-        const sourcePath = path.join(sourceDir, file);
-        console.log(`moving artifact: ${sourcePath}`);
-        const terms = file.split('.');
-        if (terms.pop() !== 'node') {
-            console.error(`non node file found: ${file}`);
+    const platform = terms.pop()
+    if (!platform) {
+        console.error(`can't find platform for: ${file}`);
+        return;
+    }
+
+    if (!outDirs.includes(platform)) {
+        console.error(`invalid platform: ${platform} for file: ${file}`);
+        return;
+    }
+
+    const destPath = path.join(npmDir, platform, path.parse(file).base);
+
+    fs.copyFile(file, destPath, (copyErr) => {
+        if (copyErr) {
+            console.error(`Could not copy the file: ${copyErr}`);
             return;
         }
-        const platform = terms.pop()
-        if (!platform) {
-            console.error(`can't find platform for: ${file}`);
-            return;
-        }
 
-        if (!outDirs.includes(platform)) {
-            console.error(`invalid platform: ${platform} for file: ${file}`);
-            return;
-        }
-
-        const destPath = path.join(npmDir, platform, file);
-
-        fs.copyFile(sourcePath, destPath, (copyErr) => {
-            if (copyErr) {
-                console.error(`Could not copy the file: ${copyErr}`);
-                return;
-            }
-
-            console.log(`Copied ${sourcePath} to ${destPath}`);
-        });
-        // }
+        console.log(`Copied ${file} to ${destPath}`);
     });
 });
+
+function collectNodeBinaries(root) {
+    const files = fs.readdirSync(root, { withFileTypes: true })
+    const nodeBinaries = files
+        .filter((file) => file.isFile() && file.name.endsWith('.node'))
+        .map((file) => path.join(root, file.name))
+
+    const dirs = files.filter((file) => file.isDirectory())
+    for (const dir of dirs) {
+        nodeBinaries.push(...(collectNodeBinaries(path.join(root, dir.name))))
+    }
+    return nodeBinaries
+}
